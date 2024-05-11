773dba61d28a444cdab4e7cc5eb9154f7710945a
Mara Kudrjavceva <maara.kudrjavceva@gmail.com>
2024-05-11 15:28:50 +0300

    11

--- scripts/day11.js
+++ scripts/day11.js
@@ -40,6 +40,42 @@ while (k <= stop){ //at 250 we will still run the loop because we are using <= s
 //after iteration
 console.log("After while loop k is", k);
 
+//do while loops or what I call "Shoot first ask question later"
+
+let m = 0;
+//let's get paragraph with id of "doWhileLoopOutput"
+//and add same text as on console but with <br> tags
+//so that it is displayed as a list
+const doWhileLoopOutput = document.getElementById ("doWhileLoopOutput");
+do {
+    console.log("Do while loop iteration", m);
+    doWhileLoopOutput.innerHTML += `Do while loop iteration ${m}<br>`;//notice the += here
+    //also notice backticks instead of ' or " this is called template string
+    //if we use  backtick we can use $(variable) to insert variable value into the string
+    //again innerHTML is a string so we can concatenate strings with += operator
+    //above is very much a quick and dirty way to add text to a paragraph
+    //better way would be to use document.createElement and appendChild // which we wild do in the followup lectures
+
+    m++;
+} while (m < 5);//notice the semicolon here
+//do while loop will always run at least once because the condition is checked after the first iteration
+console.log ("After do while loop m is", m);
+
+//If you did not know about do while loop you could do the same thing with while loop using break
+
+let n = 9000;
+while (true){ //infinite loop
+    console.log("While loop iteration", n); // this will run at least once
+    doWhileLoopOutput.innerHTML += `While loop iteration ${n}<br>`;
+    n++;//also will run at least once
+    if (n >= 5){//condition to break out of the loop
+        console.log("I want to break free because n is", n);
+        break; //break out of the loop and continue right after while loop
+    }
+    //you could add multiple break conditions
+}
+console.log("After break while loop is", n);
+
 //since it is kind of painful to remember to increment or decrement the loop variable
 //for loops have been added to many programming languages they are more concise and less error prone
 
@@ -47,21 +83,28 @@ console.log("After while loop k is", k);
 //for example we might get some user input or we might be reading from a file or a network and something might change
 //for loops should be used when we know how many iterations we need
 
-//do while loops or what I call "Shoot first ask question later"
-let m = 0;
-//let's get paragraph with id of "doWhileLoopOutput"
-//and add same text as on console but with <br> tags
-//so that it is displayed as a list
-const doWhileLoopOutput = document.getElementById ("doWhileLoopOutput");
-do{
-    console.log("Do while loop iteration", m);
-    doWhileLoopOutput.innerHTML += 'Do while loop iteration ${m}<br>'; 
+//for loop syntax
+//for (start; stop; step) {code block} 
+//start is executed only once at the bbeginning
+//stop is checked before each iteration
+//step is executed after each iteration
 
+//for loop is a combination of while loop and incrementing the loop variable
 
+for (let i = 100; i < 103; i++){//we can declare p in the for loop itself
+    console.log("For loop iteration", i);
+}
+//after iteration the loop i is not available!! 
+//Older versions of JavaScript would have made it available
+//in other word i went out of scope - the scope of loop variable is only inside the loop
+//this is GOOD thing because we do not want to accidentally use the loop variable outside of the loop
 
-    m++;
-} while (m < 5);//notice the semicolon here
-//do while loop will always run at least once because the condition is checked after the first iteration
-console.log ("After do while loop m is", m)
+console.log("After for loop i is", i);//this will show the OTHER i not the one from for loop 
+//if we did not have other i it would be an error
 
-//if you did not know about do while loop 
+//lets use i again but now we will count down by 2
+for (let i = 10; i >= 0; i-+2){//
+    console.log("For loop iteration", i);
+}
+//again i is not available here, unless we have it declared out side the loop
+console.log("After for loop i is", i);//still 5 from the while loop
